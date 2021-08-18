6bcb1039f9bb07bbcf899516c038a0c88189ef7f
Daniel Arellano <darellanoa1300@alumno.ipn.mx>
2021-08-18 17:00:01 -0500

    Cuarto push a rama master del avance portafolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,7 +93,18 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevE1: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+})
 
 /*==================== TESTIMONIAL ====================*/
 
